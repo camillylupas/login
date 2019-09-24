8adc9af5bf5364068d904bf50e4b4339e7707c89
camillylupas <camilly_l_oliveira@estudante.sc.senai.br>
2019-09-24 19:52:07 -0300

    criado o index.php
    iniciado o layout bootstrap

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<!--
+
+     para acessar o projeto, habilite o Apache  http://localhost/login/
+-->
+<head>
+    <!-- Meta tags Obrigatórias -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<title>sistema de login</title>
+</head>
+
+<body class="bg-dark">
+    
+    <main class="container mt-4">
+     <section class="row">
+       <div class="col-lg-4 offset-lg-4"id="alerta">
+           <div class="alert alert-success text-center">
+               <strong class="resultado">
+                   alo ha tchurmaaaaaaaaa
+               </strong>
+
+           </div>
+
+       </div>
+     </section>
+     <!
+     
+     <section class="row">
+        <div class="col-lg-4 offset-lg-4 bg-light rounded "id="caixalogin">
+        <h2 class="text-center mt-2">
+            Entrar no sistema
+        </h2>
+        <form action="#" method="post" class="p-2"
+        id="formLogin">
+        <div class="form-group">
+            <input type="text"name="" id="">
+
+        </div>
+
+        </form>
+        
+
+        </div>      
+        </section>
+    
+    </main>
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>

754dfbe4a9f4698370135af58d4ae661e9fd36fc
camillylupas <camilly_l_oliveira@estudante.sc.senai.br>
2019-09-30 21:54:46 -0300

    formulario de cadastro de novo usuario
    close#3

--- index.php
+++ index.php
@@ -13,9 +13,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Sistema de Login</title>
     <style>
-    #alerta,#caixaSenha,#caixaRegistro{
-        display: none;
-    }
+        #alerta,
+        #caixaSenha,
+        #caixaRegistro {
+            display: none;
+        }
     </style>
 </head>
 
@@ -137,8 +139,7 @@
                     </div>
                     <div class="form-group">
                         <div class="custom-control custom-checkbox">
-                            <input type="checkbox" name="concordar" id="concodar" 
-                            class="custom-control-input">
+                            <input type="checkbox" name="concordar" id="concodar" class="custom-control-input">
                             <label for="concordar" class="custom-control-label">
                                 eu concordo com <a href="#"> o termos e condições.</a>
 
@@ -147,8 +148,7 @@
                     </div>
 
                     <div class="form-group">
-                        <input type="submit" value="::Registrar::" name="btnRegistrar" 
-                        id="btnRegistrar" class="btn btn-primary btn-block">
+                        <input type="submit" value="::Registrar::" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
 
                         <div class="form-group">
                             <p class="text-center">
@@ -176,6 +176,33 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        // código jQuery para mostrar e ocultar os formulários
+        $(function() {
+        $("#btnEsqueci").click(function() {
+        $("#caixalogin").hide(); //ocultar
+        $("caixaSenha").show(); //mostrar
+        });
+
+        $("btnJaRegistrado").click(function()){
+            $("#caixaSenha").hide();
+            $("#caixaRegistar).show();
+        });
+
+        $("#btnRegistarNovo").click(function() {
+        $("#btnRegistrarNovo").hide();
+        $("#btnRegistrar2").show();
+
+        });
+
+
+
+
+        })
+        });
+
+        });
+    </script>
 </body>
 
 </html>

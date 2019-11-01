cb7659fe7019d4a98de76c3295422c48e5be7981
camillylupas <camilly_l_oliveira@estudante.sc.senai.br>
2019-11-01 20:49:01 -0200

    avaliaçao

--- session.php
+++ session.php
@@ -1,10 +1,21 @@
 <?php
 session_start();
-require_once "configBD.php";
-if(isset($_SESSION['nomeDoUsuario'])){
-    //Logado 
-}else{
-    //Se não estiver logado, redirecionar para index
+require_once "configbd.php";
+if (isset($_SESSION['nomeDoUsuario'])) {
+    //logado
+    $usuario = $_SESSION['nomeDoUsuario'];
+    $sql = $connect->prepare("SELECT * FROM usuario WHERE nomeUsuario = ?");
+    $sql->bind_param("s", $usuario);
+    $sql->execute();
+    $resultado = $sql->get_result();
+    $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+    $nomeDoUsuario = $linha['nomeUsuario'];
+    $nomeCompleto  = $linha['nomeCompleto'];
+    $emailUsuario  = $linha['emailUsuario'];
+    $dataCriado    = $linha['dataCriado'];
+    //conversão de data
+    $dataCriado = date('d/m/Y', strtotime($dataCriado));
+} else {
+    //se não estiver logado, redirecionar para o index
     header("location: index.php");
 }
-© 2019 GitHub, Inc.
